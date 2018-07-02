aa5af1b612898dc072810490d25201f62755c6bf
Unknown <boydomzkie@gmail.com>
2018-07-02 11:47:21 +0800

    update live

--- wp-content/themes/owlquest-child/fullwidth-home.php
+++ wp-content/themes/owlquest-child/fullwidth-home.php
@@ -50,13 +50,132 @@ get_header(); ?>
 		</div><!-- .row -->
 	</div><!-- .container -->
 </div><!-- #content -->
-	
-	<div class="owlnewsletter">
+
+
+<div class="howitworks">
+	<h1>How it works</h1>
+
+	<div class="grey">
 		<div class="container">
-			<h1>Newsletter</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+			<div class="row">
+				<div class="col-md-2">&nbsp;</div>
+				<div class="col-md-8">
+
+
+						<div class="onef">
+							<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/1.png" alt="" width="" height="" />					
+						</div>
+
+						<div class="threef">
+							<h4>Get Started</h4>
+							<p>consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+						</div>
+
+						<div class="cls"></div>
+
+						<div class="triangle downg"></div>
+				</div>
+			</div>
+
+		</div>		
+	</div>
+
+	<div class="light">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2">&nbsp;</div>
+				<div class="col-md-8">
+
+
+						<div class="onef">
+							<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/2.png" alt="" width="" height="" />					
+						</div>
+
+						<div class="threef">
+							<h4>Requirements</h4>
+							<p>consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+						</div>
+
+						<div class="cls"></div>
+						<div class="triangle downl"></div>
+				</div>
+			</div>
+		</div>		
+	</div>
+
+	<div class="grey">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2">&nbsp;</div>
+				<div class="col-md-8">
+
+
+						<div class="onef">
+							<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/3.png" alt="" width="" height="" />					
+						</div>
+
+						<div class="threef">
+							<h4>Processing</h4>
+							<p>consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+						</div>
+
+						<div class="cls"></div>
+						<div class="triangle downg"></div>
+				</div>
+			</div>
+		</div>		
+	</div>
+
+	<div class="light">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2">&nbsp;</div>
+				<div class="col-md-8">
+
+
+						<div class="onef">
+							<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/4.png" alt="" width="" height="" />					
+						</div>
+
+						<div class="threef">
+							<h4>Get Started</h4>
+							<p>consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+						</div>
+
+						<div class="cls"></div>
+				</div>
+			</div>
+		</div>		
+	</div>
+				
+</div>						
+	
+<div class="owlnewsletter">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-2">&nbsp;</div>
+			<div class="col-md-8">
+			
+				<h1>Newsletter</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+				
+				<form class="newsletter" name="newsletter">
+					<div class="row">						
+						<div class="col-md-2">&nbsp;</div>
+						<div class="col-md-6">
+							<input class="txtemail" type="email" placeholder="Your Email" size="30" required title="Your Email" />								
+							<!--<div class="cls"></div>-->
+						</div>
+						<div class="col-md-2">
+							<input class="btnsubmit" type="submit" value="Subscribed">
+						</div>	
+					</div>
+				</form>					
+
+			</div>			
 		</div>
 	</div>
+</div>
 
 <?php
 get_footer();
--- wp-content/themes/owlquest-child/header.php
+++ wp-content/themes/owlquest-child/header.php
@@ -28,13 +28,15 @@
         <div class="container">
             <nav class="navbar navbar-expand-xl p-0">
                 <div class="navbar-brand">
-                    <?php if ( get_theme_mod( 'wp_bootstrap_starter_logo' ) ): ?>
+                    <img class="logo" src="https://www.owlquest.ca/wp-content/uploads/2018/06/Owlquestlogo.png" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
+                    <?php /*if ( get_theme_mod( 'wp_bootstrap_starter_logo' ) ): ?>
                         <a class="logo" href="<?php echo esc_url( home_url( '/' )); ?>">
-                            <img src="<?php echo esc_attr(get_theme_mod( 'wp_bootstrap_starter_logo' )); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
+                            <!--<img src="<?php echo esc_attr(get_theme_mod( 'wp_bootstrap_starter_logo' )); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">-->
+                            <img src="https://www.owlquest.ca/wp-content/uploads/2018/06/Owlquestlogo.png" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
                         </a>
                     <?php else : ?>
                         <a class="site-title" href="<?php echo esc_url( home_url( '/' )); ?>"><?php esc_url(bloginfo('name')); ?></a>
-                    <?php endif; ?>
+                    <?php endif;*/ ?>
 
                 </div>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-nav" aria-controls="" aria-expanded="false" aria-label="Toggle navigation">
@@ -59,29 +61,7 @@
         </div>
 	</header><!-- #masthead -->
     <?php if(is_front_page() && !get_theme_mod( 'header_banner_visibility' )): ?>
-        <div id="page-sub-header" <?php if(has_header_image()) { ?>style="background-image: url('<?php header_image(); ?>');" <?php } ?>>
-            <div class="container">
-                <h1>
-                    <?php
-                    if(get_theme_mod( 'header_banner_title_setting' )){
-                        echo get_theme_mod( 'header_banner_title_setting' );
-                    }else{
-                        echo 'Wordpress + Bootstrap';
-                    }
-                    ?>
-                </h1>
-                <p>
-                    <?php
-                    if(get_theme_mod( 'header_banner_tagline_setting' )){
-                        echo get_theme_mod( 'header_banner_tagline_setting' );
-                }else{
-                        //echo esc_html__('To customize the contents of this header banner and other elements of your site, go to Dashboard > Appearance > Customize','wp-bootstrap-starter');
-                    }
-                    ?>
-                </p>
-                <!-- <a href="#content" class="page-scroller"><i class="fa fa-fw fa-angle-down"></i></a> -->
-            </div>
-        </div>
+        
     <?php endif; ?>
 	<div id="content" class="site-content">
 		<div class="container">
